50838809bca367c7846aebb0c6c51e9655e04a2d
FarazRazi <72218210+FarazRazi@users.noreply.github.com>
2022-12-03 00:23:45 +0500

    final commit

--- src/App.js
+++ src/App.js
@@ -1,41 +1,213 @@
-import logo from "./logo.svg";
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardHeader,
+  createTheme,
+  Grid,
+  TextField,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { useEffect, useState } from "react";
-import { getSections } from "./firebase";
 import "./App.css";
+import { getSections, getSwaps, publishSwap } from "./firebase";
+const theme = {
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+};
+function ValidateEmail(input) {
+  var validRegex = /[a-z]+\d\d\d\d\d\d+@nu.edu.pk/;
+
+  if (input.match(validRegex)) {
+    // alert("Valid email address!");
+    return true;
+  } else {
+    alert("Invalid email address!");
+    return false;
+  }
+}
+function openMail(email) {
+  window.open(
+    `mailto:${email}?subject=Section Change Request&body=Can we talk about section change?`
+  );
+}
 
 function App() {
   const [sections, setSections] = useState();
-  const getAllSections = () => {};
+  const [swaps, setSwaps] = useState();
+  const [fromSection, setFromSection] = useState();
+  const [toSection, setToSection] = useState();
+  const [email, setEmail] = useState();
   useEffect(() => {
     try {
       getSections().then((u) => {
-        console.log(u);
+        // console.log(u);
         setSections(u);
       });
     } catch (error) {
       console.log(error.message);
     }
   }, [setSections]);
+  const askSwap = (e) => {
+    if (ValidateEmail(email) && fromSection && toSection) {
+      publishSwap({
+        email: email,
+        toSection: toSection,
+        fromSection: fromSection,
+      });
+      setEmail();
+      setFromSection();
+      setToSection();
+    }
+  };
+  useEffect(() => {
+    try {
+      if (fromSection && toSection)
+        getSwaps(fromSection, toSection).then((u) => {
+          // console.log(u);
+          setSwaps(u);
+        });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [fromSection, toSection]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ul>
-          {sections && sections.map((section) => <li>{section.name}</li>)}
-        </ul>
-      </header>
-    </div>
+    <ThemeProvider theme={createTheme(theme)}>
+      <div className="App">
+        <header className="App-header">
+          <Typography variant="h1" marginTop={5}>
+            Section Swapper
+          </Typography>
+          <Grid container align="center" justifyContent="center" spacing={2}>
+            <Grid item>
+              <TextField
+                label="Email"
+                value={email}
+                helperText="Please Input Your valid NU email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item marginTop={1}>
+              <Button variant="contained" onClick={() => askSwap()}>
+                Ask For Swap
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Typography variant="body" marginY={5}>
+            {(fromSection ? fromSection : "From") +
+              " -> " +
+              (toSection ? toSection : "To")}
+          </Typography>
+          <Typography variant="body" marginBottom={5}>
+            Select your sections and check for any swaps available below.
+          </Typography>
+          <Grid container align="center" justifyContent="center" spacing={2}>
+            <Grid item xs={4}>
+              <Grid marginY={2}>
+                <Card sx={{ backgroundColor: "#48a6de" }} variant="outlined">
+                  <CardHeader title="From" />
+                </Card>
+              </Grid>
+              <Grid container align="center" spacing={2}>
+                {sections &&
+                  sections.map((section, index) => (
+                    <Grid item key={index} xs={12}>
+                      <Card
+                        sx={{
+                          backgroundColor:
+                            fromSection === section.name ? "green" : "#0000",
+                        }}
+                      >
+                        <CardActionArea
+                          onClick={() =>
+                            toSection !== section.name
+                              ? setFromSection(section.name)
+                              : null
+                          }
+                        >
+                          <CardHeader title={section.name} />
+                        </CardActionArea>
+                      </Card>
+                    </Grid>
+                  ))}
+              </Grid>
+            </Grid>
+            <Grid item xs={4}>
+              <Grid marginY={2}>
+                <Card sx={{ backgroundColor: "#48a6de" }}>
+                  <CardHeader title="To" />
+                </Card>
+              </Grid>
+              <Grid container align="center" spacing={2}>
+                {sections &&
+                  sections.map((section, index) => (
+                    <Grid item key={index} xs={12}>
+                      <Card
+                        sx={{
+                          backgroundColor:
+                            toSection === section.name ? "green" : "#0000",
+                        }}
+                      >
+                        <CardActionArea
+                          onClick={() =>
+                            fromSection !== section.name
+                              ? setToSection(section.name)
+                              : null
+                          }
+                        >
+                          <CardHeader title={section.name} />
+                        </CardActionArea>
+                      </Card>
+                    </Grid>
+                  ))}
+              </Grid>
+            </Grid>
+            <Grid item xs={10} margin={2}>
+              <Typography variant="h3">Available Swaps</Typography>
+              <Grid container spacing={2} marginY={3}>
+                {swaps &&
+                  swaps.map((swap, index) => (
+                    <Grid item key={index} xs={12}>
+                      <Card>
+                        <CardHeader
+                          title={swap.email}
+                          subheader={swap.fromSection + " -> " + swap.toSection}
+                          action={
+                            <Button onClick={() => openMail(swap.email)}>
+                              Email
+                            </Button>
+                          }
+                        />
+                      </Card>
+                    </Grid>
+                  ))}
+              </Grid>
+              {swaps.length === 0 && (
+                <Typography align="center">Sorry No Swaps founds</Typography>
+              )}
+            </Grid>
+          </Grid>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  setDoc,
+  where,
+  doc,
+} from "firebase/firestore/lite";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -26,7 +35,27 @@ const db = getFirestore(app);
 
 export const getSections = async () => {
   const sectionsCol = collection(db, "sections");
-  const sectionsSnapshot = await getDocs(sectionsCol);
+  const q = query(sectionsCol, orderBy("name"));
+  const sectionsSnapshot = await getDocs(q);
   const sectionsList = sectionsSnapshot.docs.map((doc) => doc.data());
   return sectionsList;
+};
+export const publishSwap = async (data) => {
+  await setDoc(doc(db, "swaps", data.email), {
+    email: data.email,
+    fromSection: data.fromSection,
+    toSection: data.toSection,
+  });
+  //   await addDoc(collection(db, "swaps",
+};
+export const getSwaps = async (from, to) => {
+  const swapsCol = collection(db, "swaps");
+  const q = query(
+    swapsCol,
+    where("fromSection", "==", to),
+    where("toSection", "==", from)
+  );
+  const swapsSnapshot = await getDocs(q);
+  const swapsList = swapsSnapshot.docs.map((doc) => doc.data());
+  return swapsList;
 };
